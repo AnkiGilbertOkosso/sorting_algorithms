f3702e27d9e29f06ed4081427419823c1ab51a84
Anki Gilbert Okosso <ankigilberokosso@gmail.com>
2023-07-25 04:52:19 -0700

    insertion sort arranged

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,17 +8,17 @@
  */
 void swap_node(listint_t **head, listint_t **node1, listint_t *node2)
 {
-    (*node1)->next = node2->next;
-    if (node2->next != NULL)
-        node2->next->prev = *node1;
-    node2->prev = (*node1)->prev;
-    node2->next = *node1;
-    if ((*node1)->prev != NULL)
-        (*node1)->prev->next = node2;
-    else
-        *head = node2;
-    (*node1)->prev = node2;
-    *node1 = node2->prev;
+	(*node1)->next = node2->next;
+	if (node2->next != NULL)
+		node2->next->prev = *node1;
+	node2->prev = (*node1)->prev;
+	node2->next = *node1;
+	if ((*node1)->prev != NULL)
+		(*node1)->prev->next = node2;
+	else
+		*head = node2;
+	(*node1)->prev = node2;
+	*node1 = node2->prev;
 }
 
 /**
@@ -33,7 +33,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *current, *prev_insert, *next_node;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
-    	return;
+		return;
 
 	for (current = (*list)->next; current != NULL; current = next_node)
 	{
